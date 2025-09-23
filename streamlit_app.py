08602fc7c3d62c97a01c7efe7c3abcab6b134be4
rafecchang <105248016+rafecchang@users.noreply.github.com>
2025-09-22 17:25:49 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col 
 import requests
+import pandas as pd
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 
